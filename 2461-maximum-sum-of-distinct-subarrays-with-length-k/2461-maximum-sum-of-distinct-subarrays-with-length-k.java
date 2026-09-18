b5573488f5da2708369973e3335f5ecaf5e9ce81
Prabhu Siddarth A V <PRABHUSIDDARTH37@GMAIL.COM>
2026-09-18 09:32:22 +0530

    Time: 32 ms (92.71%), Space: 98.4 MB (83.38%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,32 +1,24 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        int n = nums.length;
-        if (n < k) return 0;
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        long currentSum = 0;
+        HashSet <Integer> set = new HashSet<>();
+        int left = 0;
+        long sum = 0;
         long maxSum = 0;
-        for (int i = 0; i < k; i++) {
-            currentSum += nums[i];
-            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
-        }
-        if (freq.size() == k) {
-            maxSum = currentSum;
-        }
-        for (int right = k; right < n; right++) {
-            int outgoing = nums[right - k];
-            int incoming = nums[right];
-            currentSum -= outgoing;
-            freq.put(outgoing, freq.get(outgoing) - 1);
-            if (freq.get(outgoing) == 0) {
-                freq.remove(outgoing);
+        for (int right=0; right<nums.length; right++){
+            while(set.contains(nums[right])){
+                set.remove(nums[left]);
+                sum = sum - nums[left];
+                left++;
             }
-            currentSum += incoming;
-            freq.put(incoming, freq.getOrDefault(incoming, 0) + 1);
-            if (freq.size() == k) {
-                maxSum = Math.max(maxSum, currentSum);
-            }
-        }
+            set.add(nums[right]);
+            sum = sum + nums[right];
+            if ((right - left + 1) == k){
+                maxSum = Math.max(sum, maxSum);
+                set.remove(nums[left]);
+                sum = sum - nums[left];
+                left++;
+            } 
 
-        return maxSum;
+        } return maxSum;
     }
 }
